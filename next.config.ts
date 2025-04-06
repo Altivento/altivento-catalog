f056b5298eedfc4263a28766c49aa57c54ebc005
gsorchin <gsorchin@gmail.com>
2025-04-06 12:04:46 -0600

    modify config for static site

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // Optional: Configure image optimization if you're using next/image
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
